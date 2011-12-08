099a798fc7c91f9d0654ee36d201f7c03c3082e0
Canna Wen <cannawen@gmail.com>
2011-12-07 21:16:54 -0500

    time test not working

--- life.c
+++ life.c
@@ -34,7 +34,12 @@ parallel_game_of_life (void * arg)
                 const int isouth = (i == args->nrows - 1) ? 0 : (i+1);
                 const int jwest = (j==0) ? (args->ncols - 1) * LDA : jrow - LDA;
                 const int jeast = (j== args->ncols - 1) ? 0 : jrow + LDA;
+*/
 
+				const int inorth = (i-1) ;
+                const int isouth = (i+1);
+                const int jwest = jrow - LDA;
+                const int jeast = jrow + LDA;
 
                 const char neighbor_count =
                     SMRT_BOARD (args->inboard, inorth, jwest) +
@@ -47,10 +52,10 @@ parallel_game_of_life (void * arg)
                     SMRT_BOARD (args->inboard, isouth, jeast);
 
                 SMRT_BOARD(args->outboard, i, jrow) = alivep (neighbor_count, SMRT_BOARD (args->inboard, i, jrow));
-                */
-                                const char neighbor_count =
+                
+                   /*             const char neighbor_count =
                     args->inboard[i-1+ jrow - LDA] +
-                    args->inboard[(i-1) + jro]) +
+                    args->inboard[(i-1) + jrow] +
                     args->inboard[ (i-1) + jrow + LDA] +
                     args->inboard[ i + jrow - LDA] +
                     args->inboard[i + jrow + LDA] +
@@ -58,7 +63,7 @@ parallel_game_of_life (void * arg)
                     args->inboard[ (i+1)+ jrow] +
                     args->inboard[ (i+1)+ jrow + LDA];
 
-               args->outboard[i+jrow] = alivep (neighbor_count, args->inboard[i+ jrow]);
+               args->outboard[i+jrow] = alivep (neighbor_count, args->inboard[i+ jrow]);*/
             }
 	    }
         SWAP_BOARDS( args->outboard, args->inboard );
